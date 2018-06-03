ea9463be3ab566a7e2db7975579933c6c1c9067a
Marcelo Soares <marceloh.web@gmail.com>
2018-06-03 00:35:17 -0300

    Continuação

--- src/br/com/ufpb/oac/uc/CacheSimulator.java
+++ src/br/com/ufpb/oac/uc/CacheSimulator.java
@@ -31,27 +31,62 @@ public class CacheSimulator {
 			System.out.println("Line: " + line);
 			System.out.println("Word: " + word);
 
-			String lineContent = FileAccess.readLine(cacheFile, Converter.binaryToDec(line));
+			System.out.println("Lendo a linha " + Converter.binaryToDec(line) + " da memória cache");
+			
+			String lineCache = FileAccess.readLine(cacheFile, Converter.binaryToDec(line));
+			
+			System.out.println(lineCache);
+			
+			String cacheTag = lineCache.substring(1, 9);
 
-			System.out.println(lineContent);
+			System.out.println(cacheTag);
 			
 			String raValue;
 
 			// Verifica se o bloco que contém a RA está na cache
 			boolean isRAInCache = true;
 
-			if (isRAInCache) {
+			if (tag.equals(cacheTag)) {
+				
+				System.out.println("CacheHit. Ler um dos 4 bytes da cache a partir da palavra");
+				
+				System.out.println("Byte carregado: " + Converter.binaryToDec(word));
+				
+				String byteValue = null;
+				
+				//O número da palavra com 2 bits é usado para selecionar um dos 4 bytes nessa linha.
+				switch (word) {
+				case "00":
+					byteValue = lineCache.substring(9, 17);
+					break;
+					
+				case "01":
+					byteValue = lineCache.substring(17, 25);
+					break;
+					
+				case "10":
+					byteValue = lineCache.substring(25, 33);
+					break;
+					
+				case "11":
+					byteValue = lineCache.substring(33, 41);
+					break;
 
-				/*
-				 * if(tag != tagCache){ System.out.println("CacheMiss"); }else{
-				 * System.out.println("CacheHit"); Ler posição }
-				 */
+				default:
+					break;
+				}
 
-				// raValue =
-
+				System.out.println("Valor do byte em binário: " + byteValue);
+				
+				System.out.println("Valor do byte em decimal: " + Converter.binaryToDec(byteValue));
+				
 			} else {
+				
+				System.out.println("CacheMiss");
+				
+				String tagLine = tag + line + "00";
 
-				System.out.println("Acessa à memória principal");
+				System.out.println("Acessa à memória principal no endereço " + tagLine);
 
 				System.out.println("Armazena bloco da memória da cache");
 
@@ -59,7 +94,7 @@ public class CacheSimulator {
 
 			}
 
-			System.out.println("Entrega linha da RA para a cpu");
+			System.out.println("Entrega linha da RA para a CPU");
 
 			System.out.println("-------------------------\n");
 
